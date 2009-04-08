c2b26752a14cf8930b408bae22e8a6abba4591cb
juanpin <juanpin@mac.com>
2009-04-08 09:54:25 -0700

    Add controller and gretting variable

---
+++ apps/hello_world/controllers/app.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   HelloWorld.appController
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals HelloWorld */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.Object
+*/
+HelloWorld.appController = SC.Object.create(
+/** @scope HelloWorld.appController.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
---
+++ apps/hello_world/tests/controllers/app.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   HelloWorld.appController Unit Test
+// Copyright: ©2009 My Company, Inc.
+// ==========================================================================
+/*globals HelloWorld module test ok equals same stop start */
+
+module("HelloWorld.appController");
+
+// TODO: Replace with real unit test for HelloWorld.appController
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
