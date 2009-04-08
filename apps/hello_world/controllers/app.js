5160a96d18e4325f8cff34d648c96e544b00d237
juanpin <juanpin@mac.com>
2009-04-08 10:25:32 -0700

    add clock

--- apps/hello_world/controllers/app.js
+++ apps/hello_world/controllers/app.js
@@ -17,6 +17,14 @@ HelloWorld.appController = SC.Object.create(
   
   isClockShowing: NO,
   
+  tick: function() {
+    this.set('greeting', this.now()) ;
+  },
+
+  now: function() {
+    return new Date().format('hh:mm:ss');
+  },
+  
   toggleGreeting: function() {
     var currentGreeting = this.get('greeting');
     var newGreeting = (currentGreeting === 'Hello World!') ? 'I am on SproutCore!' : 'Hello World!' ;
@@ -25,7 +33,17 @@ HelloWorld.appController = SC.Object.create(
   
   isClockShowingObserver: function() {
     var isClockShowing = this.get('isClockShowing') ;
-    var newGreeting = (isClockShowing) ? 'CLOCK!' : 'Hello World!' ;
+
+    // create a timer if it does not exist already
+    if (!this._timer) this._timer = SC.Timer.schedule({
+      target: this, action: 'tick', repeats: YES, interval: 1000
+    });
+
+    // pause the timer unless the clock is showing
+    this._timer.set('isPaused', !isClockShowing) ;
+
+    // update right now
+    var newGreeting = (isClockShowing) ? this.now() : 'Hello World';
     this.set('greeting', newGreeting) ;
   }.observes('isClockShowing')
 
