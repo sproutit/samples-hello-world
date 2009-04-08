1131bd60e68e4fc9c6ced075899829c9ea007dcd
juanpin <juanpin@mac.com>
2009-04-08 10:20:31 -0700

    Setting up an observer

--- apps/hello_world/controllers/app.js
+++ apps/hello_world/controllers/app.js
@@ -15,10 +15,18 @@ HelloWorld.appController = SC.Object.create(
 
   greeting: "Hello World!",
   
+  isClockShowing: NO,
+  
   toggleGreeting: function() {
     var currentGreeting = this.get('greeting');
     var newGreeting = (currentGreeting === 'Hello World!') ? 'I am on SproutCore!' : 'Hello World!' ;
     this.set('greeting', newGreeting);
-  }
+  },
+  
+  isClockShowingObserver: function() {
+    var isClockShowing = this.get('isClockShowing') ;
+    var newGreeting = (isClockShowing) ? 'CLOCK!' : 'Hello World!' ;
+    this.set('greeting', newGreeting) ;
+  }.observes('isClockShowing')
 
 }) ;
