d27036d71eb63c251fbd4ba1196eabd61df70213
juanpin <juanpin@mac.com>
2009-04-08 09:50:02 -0700

    remove irrelevant tagname

--- apps/hello_world/english.lproj/main_page.js
+++ apps/hello_world/english.lproj/main_page.js
@@ -15,7 +15,7 @@ HelloWorld.mainPage = SC.Page.design({
     
     labelView: SC.LabelView.design({
       layout: { centerX: 0, centerY: 0, width: 100, height: 18 },
-      tagName: "h1", value: "Hello World"
+      value: "Hello World"
     })
   })
 
